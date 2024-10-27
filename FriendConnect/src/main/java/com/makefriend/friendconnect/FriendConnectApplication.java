5815414d6a9e8724a7646bfa6bc2100f58c02742
Pochana Srikar Reddy <pochasri@amazon.com>
2024-10-27 10:23:24 +0530

    Initial setup of friendsconnect

---
+++ FriendConnect/src/main/java/com/makefriend/friendconnect/FriendConnectApplication.java
@@ -0,0 +1,13 @@
+package com.makefriend.friendconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendConnectApplication.class, args);
+	}
+
+}
